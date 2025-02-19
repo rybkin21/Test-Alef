59618a0e12e05c54ddc2730415a32616f862f136
Ivan Rybkin <i.rybkin@icloud.com>
2025-02-19 16:17:47 +0300

    Initial Commit

---
+++ Test Alef/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Alef
+//
+//  Created by Ivan Rybkin on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
